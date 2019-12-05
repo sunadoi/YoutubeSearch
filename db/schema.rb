abac1119d89e3ba3e7059320f3dbb825d960c9fb
sunadoi <y.sunadoi@gmail.com>
2019-12-05 22:26:56 +0900

    Update schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_04_054654) do
-
-  create_table "youtubes", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.string "keyword"
-  end
+ActiveRecord::Schema.define(version: 2019_12_05_124948) do
 
 end
